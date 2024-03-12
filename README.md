5a30e6d4b7844b4ae36b7237bac3961b8ea7c2dd
Mario Vago Marzal <mariovagomarzal@gmail.com>
2024-03-13 00:14:23 +0100

    Add support for the `install-brew` directive

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # dotbot-brew
+
+
+This is a [Dotbot][dotbot] plugin for installing packages and casks with
+Homebrew.
+
+## Installation
+
+## Usage
+
+The plugin provides five new directives to manage Homebrew operations:
+
+- `install-brew`: Installs Homebrew in your machine if it is not already
+  installed.
+- `brew`: Installs a package with Homebrew.
+- `cask`: Installs a cask with Homebrew.
+- `tap`: Adds a tap to Homebrew.
+- `brewfile`: Installs packages and casks from a Brewfile.
+
+In the following subsections we will describe each directive in detail and
+its available options. Additionally, we have included a section with some
+examples of how to use the plugin.
+
+### General options
+
+Every directive has a set of common options which are the following:
+
+- `stdin`: A boolean that indicates if the command stdin should be redirected
+  to the terminal.
+- `stdout`: A boolean that indicates if the command stdout should be
+  redirected to the terminal.
+- `stderr`: A boolean that indicates if the command stderr should be
+  redirected to the terminal.
+- `force-intel`: A boolean that indicates if the command should be executed
+  with Rosetta 2 on Apple Silicon Macs.
+
+They can be defined in three ways:
+
+- As a global option for every `dotbot-brew` directive. This can be done by
+  setting the defaults of the `homebrew` key. For example:
+
+  ```yaml
+  - defaults:
+      homebrew:
+        # These are the defaults
+        stdin: true
+        stdout: true
+        stderr: true
+        forece-intel: false
+  ```
+- As a gloval option for every directive of the specified type. For
+  example:
+
+  ```yaml
+  - defaults:
+      install-brew:
+        # Same as the global defaults
+        stdin: true
+        stdout: true
+        stderr: true
+        forece-intel: false
+        # ... other specific options for this directive
+  ```
+- As a local option for a specific directive. For example:
+  
+  ```yaml
+  - install-brew:
+      # Same as the global defaults
+      stdin: true
+      stdout: true
+      stderr: true
+      forece-intel: false
+      # ... other specific options for this directive
+  ```
+
+The order of preference of the options is ascending, meaning that the local
+options are prioritized over the globals of the directive, and the globals
+of the directive over the globals of the plugin.
+
+### The `install-brew` directive 
+
+The `install-brew` directive is used to install Homebrew in your machine if
+it is not already installed. The contents of this directive can be either a
+bool indicating if Homebrew should be installed or a dictionary with the
+general options, in which case the installation will be performed.
+
+## Examples
+
+## License
+
+This project is licensed under the [MIT LICENSE](/LICENSE) by Mario Vago
+Marzal.
+
+<!-- External links -->
+[dotbot]: https://github.com/anishathalye/dotbot
---
+++ brew.py
@@ -0,0 +1,132 @@
+"""Brew plugin for Dotbot"""
+import os
+import subprocess
+from pathlib import Path
+from typing import Any
+
+from dotbot import Plugin
+
+
+class Brew(Plugin):
+    """Brew plugin, a subclass of the base Plugin class"""
+
+    # The global defaults of all the directives
+    _global_defaults: dict[str, Any] = {
+        "stdin": True,
+        "stdout": True,
+        "stderr": True,
+        "force-intel": False,
+    }
+
+    # A dictionary of the directives, where each item contains a tuple with
+    # the callable that handles the directive and a dict with its specific
+    # default options.
+    _directives: dict[str, tuple[str, dict[str, Any]]] = {
+        # The `install-brew` directive
+        "install-brew": (
+            self._install_brew,
+            {},
+        ),
+    }
+
+    def can_handle(self, directive: str) -> bool:
+        """Returns wether the directive can be handled by this plugin"""
+        return directive in self._directives.keys()
+
+    def _get_directive_options(
+        self,
+        directive: str,
+        data: Any
+    ) -> dict[str, Any]:
+        """Returns the default options for the directive"""
+        options = self._global_defaults.copy()
+
+        # If the configuration has specific defaults for the plugin, we
+        # update the options dictionary with them
+        options.update(self._context.defaults().get("homebrew", {}))
+
+        # If the directive has specific global defaults, we update the
+        # options dictionary with them
+        global_directive_defaults = self._directives[directive][1]
+        global_directive_defaults.update(
+            self._context.defaults().get(directive, {})
+        )
+        options.update(global_directive_defaults)
+
+        # If the directive has specific defaults in the data, we update the
+        # options dictionary with them
+        if isinstance(data, dict):
+            options.update(data)
+
+        return options
+
+    def handle(self, directive: str, data: Any) -> bool:
+        """Handles the directive and returns wether it was successful or
+        not"""
+        # First, we build the options dictionary for the directive
+        options = self._get_directive_options(directive, data)
+
+        # Now, we can assume that the options contains all the keys of the
+        # global defaults and the specific defaults of the directive.
+        # Each directive has to handle the `data` depending on its specific
+        # type and the options.
+        return self._directives[directive][0](data, options)
+
+    def _invoke_shell_commands(
+        commands: list[str],
+        options: dict[str, Any],
+    ) -> int:
+        """"""
+        commands_str = " && ".join(commands)
+        return subprocess.call(
+            commands_str,
+            shell=True,
+            cwd=self._context.base_directory(),
+            stdin=subprocess.PIPE if not options["stdin"] else None,
+            stdout=subprocess.PIPE if not options["stdout"] else None,
+            stderr=subprocess.PIPE if not options["stderr"] else None,
+        )
+
+    def _brew_prefix(self, for_cmd: bool = True) -> str:
+        """Returns the Homebrew prefix based on the OS and the
+        architecture"""
+        os_name = os.uname().sysname
+        os_arch = os.uname().machine
+
+        if os_name == "Darwin":
+            if os_arch == "arm64" and options["force-intel"]:
+                if for_cmd:
+                    return "arch -x86_64 /usr/local/bin/brew"
+                else:
+                    return "/usr/local/bin/brew"
+            elif os_arch == "arm64" and not options["force-intel"]:
+                return "/opt/homebrew/bin/brew"
+            else:
+                return "/usr/local/bin/brew"
+        elif os_name == "Linux":
+            return "/home/linuxbrew/.linuxbrew/bin/brew"
+        else:
+            raise ValueError(f"Unsupported OS: {os_name}")
+
+    def _install_brew(
+        data: Any,
+        options: dict[str, Any],
+    ) -> bool:
+        """Installs Homebrew"""
+        if isinstance(data, dict) or (isinstace(data, bool) and data):
+            # Check if Homebrew is already installed
+            if Path(self._brew_prefix(for_cmd=False)).exists():
+                self._log.debug("Homebrew is already installed")
+                return True
+            else:
+                # Install Homebrew
+                return self._invoke_shell_commands(
+                    [
+                        "arch -x86_64" if options["force-intel"] else ""
+                        "/bin/bash -c"
+                        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)",
+                    ],
+                    options,
+                ) == 0
+        else:
+            return True
